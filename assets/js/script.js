8472727a17ca1a4cb3ebe6fbfc342581bfe39968
Kieran Collins <kiecol@protonmail.com>
2023-08-25 22:34:14 +0000

    Adjust layout for sections, change direction for each row

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
     // Burger Menu for Navbar
     var navbarToggler = document.querySelector(".navbar-toggler");
     var navbarCollapse = document.querySelector(".navbar-collapse");
     
-    navbarToggler.addEventListener("click", function() {
+    navbarToggler.addEventListener("click", function(e) {
+        // Preventing the default Bootstrap behavior (if any)
+        e.stopPropagation();
         navbarCollapse.classList.toggle("show");
     });
+
+    // Function to handle the custom click on horizontal navigation dots
+    function handleHorizontalNavClick(event) {
+        var clickedPosition = event.clientX;
+        var navWidth = event.currentTarget.offsetWidth;
+        var totalSlides = event.currentTarget.querySelectorAll('span').length;
+        var slideToGo = Math.ceil((clickedPosition / navWidth) * totalSlides);
+        
+        // Assuming this is for the "About Me" section which is the 2nd section in your structure
+        // Modify this if the order changes
+        fullpage_api.moveTo(2, slideToGo);
+    }
 
     // Initialize fullpage.js
     new fullpage('#fullpage', {
@@ -25,7 +40,18 @@ document.addEventListener("DOMContentLoaded", function() {
             if (activeNavLink) {
                 activeNavLink.classList.add('active');
             }
+        },
+        afterRender: function() {
+            // This function gets called once FullPage.js has been initialized and rendered.
+
+            // Add an ID to the horizontal navigation for targeting purposes.
+            var horizontalNavs = document.querySelectorAll('.fp-slidesNav');
+            horizontalNavs.forEach(function(nav, index) {
+                nav.id = 'horizontalNav' + (index + 1);
+
+                // Attach the click event to the horizontal navigation
+                nav.addEventListener('click', handleHorizontalNavClick);
+            });
         }
     });
 });
-
